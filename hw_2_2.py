9e40ba07f6cba4e451a9af0126e1e14b657fbfdd
Miho1991 <47381917+Miho1991@users.noreply.github.com>
2019-02-22 16:27:22 +0200

    Update hw_2_2.py

--- hw_2_2.py
+++ hw_2_2.py
@@ -7,14 +7,18 @@ while True:
     if n.isdigit():
         break;
     else:
-        print('Формат ввода не правильный! Введите число!')
+        print('Формат ввода не правильный! Введите порядковый номер числв ряда Фибоначчи!')
 
 f1 = 0
 f2 = 1
-
-for i in range (int(n)-1):
-    fn = f1+f2
-    f1 = f2
-    f2 = fn
-
+fn = 0
+if int(n)==0:
+    fn=0
+elif int(n)==1:
+    fn=1
+else:
+    for i in range (int(n)-1):
+        fn = f1+f2
+        f1 = f2
+        f2 = fn
 print ('Число [{}] ряда Чисел Фибоначчи - {}'.format(n,fn))
